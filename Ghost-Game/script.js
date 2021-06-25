486a2e29b37a538c6308bdb95de8de8ed993ebb6
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-06-24 20:55:21 -0700

    Update script.js

--- Ghost-Game/script.js
+++ Ghost-Game/script.js
@@ -7,12 +7,52 @@
 
   function reset() {
     game = {
-      ghosts: 0
+      lastTick: Date.now(),
+      ghosts: 0,
+      autoclickers: 0,
+      autoLoop: 0
     };
+  }
+  
+  function save() {
+    localStorage.setItem("ghostGameSave", JSON.stringify(game));
+  }
+  
+  function load() {
+    const loadgame = JSON.parse(localStorage.getItem("ghostGameSave"));
+    if (loadgame !== null) {
+      loadGame(loadgame);
+    }
+  }
+  
+  function loadGame(loadgame) {
+    reset();
+    for (const i in loadgame) {
+      game[i] = loadgame[i];
+    }
+    const diff = Date.now() - game.lastTick;
+    console.log(diff);
+    loop(diff, true);
   }
 
   function increment() {
     game.ghosts++;
+  }
+  
+  function buyAutoclicker() {
+    if (game.ghosts >= 10 * 2 ** game.autoclickers) {
+      game.ghosts -= 10 * 2 ** game.autoclickers;
+      game.autoclickers++;
+    }
+  }
+  
+  function loop(ms, off = false) {
+    game.lastTick = Date.now();
+    game.autoLoop += ms * game.autoclickers;
+    if (game.autoLoop >= 1000) {
+      game.ghosts += Math.floor(game.autoLoop / 1000);
+      game.autoLoop %= 1000;
+    }
   }
   
   reset();
@@ -23,7 +63,8 @@
       game: game
     },
     methods: {
-      increment: increment
+      increment: increment,
+      buyAutoclicker: buyAutoclicker
     }
   });
 })();
