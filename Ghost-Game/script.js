cd35e92f163e60b6bc849ded4ecbe2fc88bffe20
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-06-24 20:46:36 -0700

    Update script.js

--- Ghost-Game/script.js
+++ Ghost-Game/script.js
@@ -2,15 +2,19 @@
   "use strict";
 
   const Vue = window.Vue;
+
   let game;
+
   function reset() {
     game = {
       ghosts: 0
     };
   }
+
   function increment() {
     game.ghosts++;
   }
+
   const vue = new Vue({
     el: "#app",
     data: {
