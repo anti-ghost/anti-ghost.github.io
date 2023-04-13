7127ccf745c94e71e6ebdf7f133ec6b131b1b23e
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2023-04-13 11:52:01 -0700

    Create ADspeedrun.js

---
+++ ADspeedrun.js
@@ -0,0 +1,12 @@
+setInterval(function() {
+  if (player.infinities.eq(0)) {
+    bigCrunchReset();
+    requestGalaxyReset();
+    softReset();
+    buyMaxTickspeed();
+    for (let i = 8; i > 0; i--) {
+      buyAsManyAsYouCanBuy(i);
+    }
+    if (Sacrifice.nextBoost().gte(2) || (player.galaxies == 2 && player.dimensionBoosts > 12)) sacrificeBtnClick();
+  }
+}, 33);
