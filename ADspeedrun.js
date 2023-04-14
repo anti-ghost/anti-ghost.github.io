0d02375c68fb59f7a65e102509e5dc724e1a528e
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2023-04-14 13:38:36 -0700

    Update ADspeedrun.js

--- ADspeedrun.js
+++ ADspeedrun.js
@@ -4,7 +4,7 @@ setInterval(function() {
     requestGalaxyReset();
     requestDimensionBoost();
     buyMaxTickSpeed();
-    for (let i = 8; i > 0; i--) {
+    for (let i = 1; i < 9; i++) {
       buyAsManyAsYouCanBuy(i);
     }
     if (Sacrifice.nextBoost.gte(2) || (player.galaxies == 2 && player.dimensionBoosts > 12)) sacrificeBtnClick();
